44970c48cafdbe3826495602030b4cb6629f15cb
Ivan Stanojevic <ivan@byteout.com>
2016-05-04 13:26:00 +0200

    added assets folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,20 +3,26 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
 var cssSources = [
-    'module/**/*.scss',
+    'assets/styles.scss',
+];
+
+var cssWatch = [
     'vendor/bootstrap/scss/bootstrap.scss',
+    'assets/mixins.scss',
     'vendor/toastr/toastr.scss',
     'vendor/simple-sidebar.scss',
-    'vendor/font-awesome/scss/font-awesome.scss'
-];
+    'vendor/font-awesome/scss/font-awesome.scss',
+    'module/bootstrap/styles.scss',
+    'module/default/styles.scss'
+]
 
 gulp.task('styles', function () {
     gulp.src(cssSources)
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('styles.css'))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./assets'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(cssSources, ['styles']);
+    gulp.watch(cssWatch, ['styles']);
 });
--- module/default/controller/default.js
+++ module/default/controller/default.js
@@ -13,7 +13,7 @@ class defaultController extends Controller {
 
         var menuToggleButton =
             '<button class="navbar-toggler" id="menu-toggle">' +
-                '<i class="fa fa-arrow-left" aria-hidden="true"></i>' +
+                '<i class="fa fa-bars" aria-hidden="true"></i>' +
             '</button>';
 
         jQuery(menuToggleButton).prependTo('header .navbar');
@@ -24,13 +24,25 @@ class defaultController extends Controller {
 
         ctrl.sidebar = jQuery('#menu-toggle').click(function () {
             jQuery('#wrapper').toggleClass('toggled');
-            jQuery(this).find('i').toggleClass("fa-arrow-left fa-arrow-right");
         });
 
         ctrl.confirm = jQuery('#confirm-dialog').click(function () {
             confirm('Press a button!', function (confirmed) {
                 console.log(confirmed);
             });
+        });
+
+        ctrl.toastr = jQuery('#toastr-test').click(function(){
+            toastr.info('Are you the 6 fingered man?')
+
+            // Display a warning toast, with no title
+            toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
+
+            // Display a success toast, with a title
+            toastr.success('Have fun storming the castle!', 'Miracle Max Says')
+
+            // Display an error toast, with a title
+            toastr.error('I do not think that word means what you think it means.', 'Inconceivable!')
         });
 
         return ctrl;
